aeff7e3227be36f97e8745029772891e90fc899d
unknown <natalia.archanjo@gmail.com>
2023-03-23 10:31:09 -0300

    foi adicionado o codigo para calcular o KB e Byte mantendo o padrão

--- calculadora-de-armazenamento.py
+++ calculadora-de-armazenamento.py
@@ -1,3 +1,5 @@
+constante_de_conversão = 1024
+
 def converterStringParaFloat(value):
     print('Valor convertido de str para float')
     return float(value)
@@ -12,12 +14,34 @@ def byteParaBit(valorASerConvertido):
     bitsCalculado = valorASerConvertido * 8
     return bitsCalculado
 
+def byteparaKB(valorASerConvertido):
+    print('valor convertido de byte para KB')
+    KBCalculado = valorASerConvertido / constante_de_conversão
+    return KBCalculado
+
+def KBparabyte(valorASerConvertido):
+    print('valor convertido de KB para byte')
+    byteCalculado = valorASerConvertido * constante_de_conversão
+    return byteCalculado
+
+print('Insira o valor a ser convertido de bit para Byte')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = bitParaByte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+print('Insira o valor a ser calculado de Byte para KB')
+entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+valorConvertido = byteparaKB(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+
+
+print('Insira o valor a ser calculado de KB para Byte')
+entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+valorConvertido = KBparabyte(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
 print('Insira o valor a ser convertido de byte para bit')
 entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
 valorConvertido = byteParaBit(entradaDoTecladoValorASerConvertido)
-print(valorConvertido)
-
-print('Insira o valor a ser convertido de bit para byte')
-entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
-valorConvertido = bitParaByte(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
