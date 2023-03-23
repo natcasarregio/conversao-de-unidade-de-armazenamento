a86fac1024499ac31e6d2129707916ae8e21c984
unknown <natalia.archanjo@gmail.com>
2023-03-23 11:27:15 -0300

    foi adicionado a função docalculo de TB para PB e de PB para TB

--- calculadora-de-armazenamento.py
+++ calculadora-de-armazenamento.py
@@ -5,76 +5,116 @@ def converterStringParaFloat(value):
     return float(value)
 
 def bitParaByte(valorASerConvertido):
-    print('Valor convertido de bit para byte')
+    print('Valor convertido para byte')
     bytesCalculado = valorASerConvertido / 8
     return bytesCalculado
 
 def byteParaBit(valorASerConvertido):
-    print('Valor convertido de byte para bit')
+    print('Valor convertido para bit')
     bitsCalculado = valorASerConvertido * 8
     return bitsCalculado
 
 def byteparaKB(valorASerConvertido):
-    print('valor convertido de byte para KB')
+    print('valor convertido para KB')
     KBCalculado = valorASerConvertido / constante_de_conversão
     return KBCalculado
 
 def KBparabyte(valorASerConvertido):
-    print('valor convertido de KB para byte')
+    print('valor convertido para byte')
     byteCalculado = valorASerConvertido * constante_de_conversão
     return byteCalculado
 
 def KBparaMB(valorASerConvertido):
-    print('valor convertido de KB para MB')
+    print('valor convertido para MB')
     MBCalculado = valorASerConvertido / constante_de_conversão
     return MBCalculado
 
 def MBparaKB(valorASerConvertido):
-    print('valor convertido de MB para KB')
+    print('valor convertido para KB')
     KBCalculado = valorASerConvertido * constante_de_conversão
     return KBCalculado
 
 def MBparaGB(valorASerConvertido):
-    print('valor convertido de MB para GB')
+    print('valor convertido para GB')
     GBCalculado = valorASerConvertido / constante_de_conversão
     return GBCalculado
 
 def GBparaMB(valorASerConvertido):
-    print('valor convertido de GB para MB')
+    print('valor convertido para MB')
     MBCalculado = valorASerConvertido * constante_de_conversão
     return MBCalculado
+
+def GBparaTB(valorASerConvertido):
+    print('valor convertido para TB')
+    TBCalculado = valorASerConvertido / constante_de_conversão
+    return TBCalculado
+
+def TBparaGB(valorASerConvertido):
+    print('valor convertido para GB')
+    GBCalculado = valorASerConvertido * constante_de_conversão
+    return GBCalculado
+
+def TBparaPB(valorASerConvertido):
+    print('valor convertido para PB')
+    PBCalculado = valorASerConvertido / constante_de_conversão
+    return PBCalculado
+
+def PBparaTB(valorASerConvertido):
+    print('valor convertido para TB')
+    TBCalculado = valorASerConvertido * constante_de_conversão
+    return TBCalculado
 
 print('Insira o valor a ser convertido de bit para Byte')
 entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
 valorConvertido = bitParaByte(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
 
-print('Insira o valor a ser calculado de Byte para KB')
+print('Insira o valor a ser convertido de Byte para KB')
 entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
 valorConvertido = byteparaKB(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
 
-print('Insira o valor a ser calculado de KB para MB')
+print('Insira o valor a ser convertido de KB para MB')
 entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
 valorConvertido = KBparaMB(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
 
-print('Insira o valor a ser calculado de MB para GB')
+print('Insira o valor a ser convertido de MB para GB')
 entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
 valorConvertido = MBparaGB(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
 
-print('Insira o valor a ser calculado de GB para MB')
+print('Insira o valor a ser convertido de GB para TB')
+entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+valorConvertido = GBparaTB(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+print('Insira o valor a ser convertido de TB para PB')
+entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+valorConvertido = TBparaPB(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+print('Insira o valor a ser convertido de PB para TB')
+entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+valorConvertido = PBparaTB(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
+
+print('Insira o valor a ser convertido de TB para GB')
+entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
+valorConvertido = TBparaGB(entradaDoTecladoValorASerConvertido)
+print(valorConvertido) 
+
+print('Insira o valor a ser convertido de GB para MB')
 entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
 valorConvertido = GBparaMB(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
 
-print('Insira o valor a ser calculado de MB para KB')
+print('Insira o valor a ser convertido de MB para KB')
 entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
 valorConvertido = MBparaKB(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
 
-print('Insira o valor a ser calculado de KB para Byte')
+print('Insira o valor a ser convertido de KB para Byte')
 entradaDoTecladoValorASerConvertido = converterStringParaFloat(input())
 valorConvertido = KBparabyte(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
