68449beed2568fc21cc58b489f3f81d9f18b809c
unknown <natalia.archanjo@gmail.com>
2023-04-27 11:17:55 -0300

    Foi adicionada uma nova variavel na função que ira calcular a conversão para bit ou saindo de bit pois o valor é diferente das outras unidades

--- calculadora-final.py
+++ calculadora-final.py
@@ -1,10 +1,10 @@
 #valores constantes e lista de unidade
 unidades_de_armazenamento = ['bit','byte','Kb','Mb','Gb','Tb','Pb']
 VALOR_CONVERSAO = 1024
-VALOR_COVERSAO_BIT = 8
+VALOR_CONVERSAO_BIT = 8
 
-print('Bem vindo(a)!Essa é uma calculadora de unidade de armazenamentos \n Aqui está as unidades que você pode usar', unidades_de_armazenamento)
-print('\nInsira os valores pedidos:')
+print('Bem vindo(a)!Essa é uma calculadora de unidade de armazenamentos \n Aqui está as unidades que você pode usar:', unidades_de_armazenamento)
+print('\n Insira os valores pedidos:')
 
 #dados inseridos
 unidade_inicial = input(" Unidade Inicial:")
@@ -15,7 +15,7 @@ else:
 
 unidade_final = input(" Unidade Final:")
 while unidade_final not in unidades_de_armazenamento:
-    unidade_final = input("Unidade invalida insira uma que tem na lista")
+    unidade_final = input("Unidade invalida insira uma que tem na lista:")
 else:
     posicao_final = unidades_de_armazenamento.index(unidade_final)
 
@@ -23,12 +23,24 @@ valor_converter = int(input(" Valor a ser convertido:"))
 
 #conversão dos valores
 def conversao ():
-    if posicao_inc >= posicao_final:
-        distancia = posicao_inc - posicao_final
-        resultado = valor_converter * (VALOR_CONVERSAO ** distancia)
-    elif   posicao_inc < posicao_final:
-        distancia = posicao_final - posicao_inc
-        resultado = valor_converter / (VALOR_CONVERSAO ** distancia)
+    if unidade_inicial == 'bit' and unidade_final != 'bit':
+        primeira_conversao = valor_converter/ VALOR_CONVERSAO_BIT
+        distancia = posicao_final - unidades_de_armazenamento.index('byte')
+        resultado = primeira_conversao/ (VALOR_CONVERSAO**distancia)
+    elif unidade_final == 'bit' and unidade_inicial != 'bit':
+        distancia = posicao_inc - unidades_de_armazenamento.index('byte')
+        primeira_conversao = valor_converter * (VALOR_CONVERSAO)
+        resultado = primeira_conversao * VALOR_CONVERSAO_BIT
+    elif unidade_final == 'bit' and unidade_inicial == 'bit':
+        resultado = valor_converter
+
+    else:
+        if posicao_inc >= posicao_final:
+            distancia = posicao_inc - posicao_final
+            resultado = valor_converter * (VALOR_CONVERSAO ** distancia)
+        elif posicao_inc < posicao_final:
+            distancia = posicao_final - posicao_inc
+            resultado = valor_converter / (VALOR_CONVERSAO ** distancia)
     print('\n O resultado da conversão é:',resultado, unidade_final)
 
 conversao()
