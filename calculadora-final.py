2f7631bf8a273f740b45c10fb892312c4d94b220
unknown <natalia.archanjo@gmail.com>
2023-04-27 09:50:38 -0300

    foi adicionado uma condição para poder encontrar a posição na lista da unidade escolida para ser feito a coversão

--- calculadora-final.py
+++ calculadora-final.py
@@ -1,3 +1,34 @@
-listaDeNumeros = ['1','2','3','4','5']
+#valores constantes e lista de unidade
+unidades_de_armazenamento = ['bit','byte','Kb','Mb','Gb','Tb','Pb']
+VALOR_CONVERSAO = 1024
+VALOR_COVERSAO_BIT = 8
 
-print(listaDeNumeros)
+print('Bem vindo(a)!Essa é uma calculadora de unidade de armazenamentos \n Aqui está as unidades que você pode usar', unidades_de_armazenamento)
+print('\nInsira os valores pedidos:')
+
+#dados inseridos
+unidade_inicial = input(" Unidade Inicial:")
+while unidade_inicial not in unidades_de_armazenamento:
+    unidade_inicial = input("Unidade invalida insira uma que tem na lista")
+else:
+    posicao_inc = unidades_de_armazenamento.index(unidade_inicial)
+
+unidade_final = input(" Unidade Final:")
+while unidade_final not in unidades_de_armazenamento:
+    unidade_final = input("Unidade invalida insira uma que tem na lista")
+else:
+    posicao_final = unidades_de_armazenamento.index(unidade_final)
+
+valor_converter = int(input(" Valor a ser convertido:"))
+
+#conversão dos valores
+def conversao ():
+    if posicao_inc >= posicao_final:
+        distancia = posicao_inc - posicao_final
+        resultado = valor_converter * (VALOR_CONVERSAO ** distancia)
+    elif   posicao_inc < posicao_final:
+        distancia = posicao_final - posicao_inc
+        resultado = valor_converter / (VALOR_CONVERSAO ** distancia)
+    print('\n O resultado da conversão é:',resultado, unidade_final)
+
+conversao()
